5b525612518fd605a20d0006c03daba1d93db0f9
MojoAlpha <mohitp@mnnit.ac.in>
2020-03-25 23:29:21 +0530

    basic features added

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-alert('connected')
+var Mynode = document.getElementsByTagName("li")
+var i;
+for(i=0 ; i<Mynode.length ; ++i){
+    var span = document.createElement("span");
+    var sym = document.createTextNode("\u00D7");
+    span.className = "delete"
+    span.appendChild(sym)
+    Mynode[i].appendChild(span)
+}
+
 function newElement(){
-    var li = document.createElement(li);
-    var input = document.getElementById(adTask).value;
+    var li = document.createElement("li");
+    var input = document.getElementById("adTask").value;
     var t = document.createTextNode(input);
     li.appendChild(t);
-    if(t=='')
+    if(input === '')
     alert('Void task cannot be submitted!!')
-    else
+    else{
     document.getElementById("UL").appendChild(li);
+    }
+
     document.getElementById("adTask").value = "";
     var span = document.createElement("SPAN");
     var txt = document.createTextNode("\u00D7");
@@ -20,4 +31,18 @@ function newElement(){
       div.style.display = "none";
     }
    }
+}
+
+var list = document.querySelector('ul')
+list.addEventListener('click',function(ev){
+    if(ev.target.tagName === 'li')
+       ev.target.classList.toggle('done');
+},false);
+
+var close = document.getElementsByClassName("delete");
+for(i=0 ; i < close.length ; ++i){
+    close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
 }
